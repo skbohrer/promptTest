c1d8cbde3995cb52f1b31a963e32ba0ab9addf5a
skbohrer <skbohrer@gmail.com>
2018-01-05 00:07:04 -0500

    hide keyboard after select event also

--- www/confirmtest.js
+++ www/confirmtest.js
@@ -13,8 +13,18 @@ function init() {
   	document.getElementById('busOff').onclick = fieldClick;
  	document.getElementById('busOn').onclick = fieldClick;
   	document.getElementById('busPOB').onclick = fieldClick;
-  	document.getElementById('busPLB').onclick = fieldClick;
+  	document.getElementById('busPLB').onclick = fieldClick;\
+
+  	document.getElementById('busID').onselect = fieldClick;
+  	document.getElementById('busOff').onselect = fieldClick;
+ 	document.getElementById('busOn').onselect = fieldClick;
+  	document.getElementById('busPOB').onselect = fieldClick;
+  	document.getElementById('busPLB').onselect = fieldClick;
+
 }
+
+
+
 
 // Wait for device API libraries to load
 document.addEventListener("deviceready", init, false);
